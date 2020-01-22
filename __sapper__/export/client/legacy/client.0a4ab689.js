c68ec87852651c0b046b8a8812fd59200f923cd2
Captain Fantasy <captain@fantasyui.com>
2020-01-21 22:53:56 -0500

    New Release

--- __sapper__/export/client/legacy/client.0a4ab689.js
+++
@@ -1,1 +0,0 @@
-import"./index.6d86eef2.js";import{s as e}from"./app.1da6607a.js";e({target:document.querySelector("#sapper")});
---
+++ docs/client/legacy/client.3b435e61.js
@@ -0,0 +1,1 @@
+import"./index.6d86eef2.js";import{s as e}from"./app.195321a5.js";e({target:document.querySelector("#sapper")});
