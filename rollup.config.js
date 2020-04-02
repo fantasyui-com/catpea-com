b6656d02f2cb0156ac166f5487378557e1289c09
Captain Fantasy <captain@fantasyui.com>
2020-04-02 12:05:11 -0400

    New Release

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
+
 import json from '@rollup/plugin-json';
 
 //import octicons from 'octicons';
@@ -43,12 +44,16 @@ export default {
         'process.browser': true,
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
+
       svelte({
+
         dev,
         hydratable: true,
         emitCss: true,
         preprocess, // <-- ADDED THIS LINE
+
       }),
+
       resolve({
         browser: true,
         dedupe
@@ -84,6 +89,7 @@ export default {
     input: config.server.input(),
     output: config.server.output(),
     plugins: [
+      json(),
       replace({
         'process.browser': false,
         'process.env.NODE_ENV': JSON.stringify(mode)
