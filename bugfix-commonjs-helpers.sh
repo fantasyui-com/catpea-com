2ca9304711e77195403ea4f91c338cb5bb23d1a7
Captain Fantasy <captain@fantasyui.com>
2020-04-10 13:37:08 -0400

    Bugfix bugfix.

--- bugfix-commonjs-helpers.sh
+++ bugfix-commonjs-helpers.sh
@@ -6,6 +6,6 @@ target=$(ls -1 docs/client/*commonjsHelpers*| head -1);
 filename=$(basename -- "$target")
 fixedname="${filename##_}"
 find ./docs -type f -exec sed -i -e "s/${filename}/${fixedname}/g" {} \;
-mv ./docs/client/$filename ./docs/client/$fixedname
+mv -f ./docs/client/$filename ./docs/client/$fixedname
 fixes=$(grep -R '_commonjsHelpers' docs/client/|wc -l);
 echo "Found ${fixes} problems after fixing. Bye."
